443ccd975592ac42f5189d0e8ed84f8fa6381755
kholliday88 <kholliday88@gmail.com>
2016-03-27 21:15:18 -0400

    Random Image is fully operational.

--- js/random.js
+++ js/random.js
@@ -1,15 +1,9 @@
-// function getRandom(callback) {
-//   var requestRandom = new XMLHttpRequest();
-//   requestRandom.open('GET', 'https://randomuser.me/api/');
-//   requestRandom.onload = function() {
-//   var response1 = JSON.parse(requestRandom.responseText);
-//     callback(response1);
-// };
-// requestRandom.send();
-// }
-//
-// window.addEventListener('load', function) {
-//   getRandom(function (picture){
-//     var picture = document
-//   })
-// }
+module.exports = function getRandom(message, callback) {
+  var requestRandom = new XMLHttpRequest();
+  requestRandom.open('GET', 'https://randomuser.me/api/');
+  requestRandom.onload = function() {
+    var response1 = JSON.parse(requestRandom.responseText);
+    callback(response1, message);
+  };
+  requestRandom.send();
+};
